bebfdc3ba912076f87f3f802fe2d17f1d20c2c07
Zhuoqian Zhou <ben02060846@gmail.com>
2023-12-05 17:21:59 +0800

    modified:   market/DataStreamLib.sol

--- market/DataStreamLib.sol
+++ market/DataStreamLib.sol
@@ -2,6 +2,25 @@
 pragma solidity ^0.8.17;
 
 library DataStreamLib {
+    // event UpdateOrder(
+    //     address indexed account, //0
+    //     bool isLong, //1
+    //     bool isIncrease, //2 if false, trade type == "trigger", otherwise, type =="limit"
+    //     uint256 orderID, //3
+    //     address market, //4 -> market name
+    //     // -------------------
+    //     // address collateralToken, //TODO: fix me multi collateral token
+    //     uint256 size, //5
+    //     uint collateral, //6
+    //     uint256 triggerPrice, //7
+    //     bool triggerAbove, // 8TODO, set to bool
+    //     uint tp, //9
+    //     uint sl, //10
+    //     uint128 fromOrder, //11, triggertp&sl, order
+    //     bool isKeepLev, //12, trigger, , order, orders
+    //     MarketDataTypes.UpdateOrderInputs params
+    // );
+
     struct OrderExec {
         address market;
         address account;

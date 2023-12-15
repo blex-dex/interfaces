f40280d4dfde16c22b6f367c99dbb8c2ef3f8347
Zhuoqian Zhou <ben02060846@gmail.com>
2023-12-15 16:21:36 +0800

    new file:   oracle/IChainPriceFeed.sol 	modified:   oracle/IPrice.sol

---
+++ oracle/IChainPriceFeed.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+interface IChainPriceFeed {
+    function PRICE_PRECISION() external view returns (uint256);
+
+    function sampleSpace() external view returns (uint256);
+
+    function priceFeeds(address token) external view returns (address);
+
+    function priceDecimals(address) external view returns (uint256);
+
+    function setSampleSpace(uint256 times) external;
+
+    function setPriceFeed(
+        address token,
+        address feed,
+        uint256 decimal
+    ) external;
+
+    function getLatestPrice(address token) external view returns (uint256);
+
+    function getPrice(
+        address token,
+        bool maximise
+    ) external view returns (uint256);
+}
